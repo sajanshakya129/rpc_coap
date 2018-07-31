aeb9c503889151bab229fb71a3997b60b539b7d8
Sajan <ersajanshakya129@gmail.com>
2018-07-31 17:15:46 +0200

    ip show format completed and class seperated to headerfile

--- main.cpp
+++ main.cpp
@@ -1,119 +1,13 @@
 #include<iostream>
-#include<string.h>
-#include <time.h>
-#include <libmnl/libmnl.h>
-#include <linux/rtnetlink.h>
-#include <linux/netlink.h>
-#include <linux/if_link.h>
-#include <linux/if_addr.h>
-#include <linux/neighbour.h>
 
-
-
+#include "network.h"
 
 using namespace std;
-
-class Network {
-private:
-	void socket(char*,int);
-
-public:
-	Network() {
-		cout << "hello from constructor" << endl;
-	}
-	void ipShow(char*);
-	// void ipRouteAdd(char*);
-	// void ipRouteShow(char*);
-	// void ipRouteDel(char*);
-};
-
-void Network::socket(char *ipType, int nlMsgType) {
-	unsigned int seq, portid;
-
-	char buf[MNL_SOCKET_BUFFER_SIZE]; //buffer
-	struct nlmsghdr *nlh; //network link message header
-	struct rtgenmsg *rt;
-	void *rt1;
-	nlh = mnl_nlmsg_put_header(buf);
-	nlh->nlmsg_type	= nlMsgType;//get information about Ip address
-	nlh->nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;// flag for request
-	nlh->nlmsg_seq = seq = time(NULL);
-	
-	
-	rt = mnl_nlmsg_put_extra_header(nlh, sizeof(struct rtgenmsg));//
-	cout<<"rt----"<<rt1<<endl;
-	cout<<"rt1---"<<rt1<<endl;
-
-	if (strcmp(ipType, "ipv4") == 0)
-		rt->rtgen_family = AF_INET;// setting IPv4
-	else if (strcmp(ipType, "ipv6") == 0)
-		rt->rtgen_family = AF_INET6;// setting IPv6
-
-	cout<<"rtttttt"<<rt;
-	struct mnl_socket *nl; //socket pointer nl
-	
-	nl = mnl_socket_open(NETLINK_ROUTE);//socket opening
-	if (nl == NULL) {
-		perror("mnl_socket_open");
-		exit(EXIT_FAILURE);
-	}
-
-	if (mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0) { //binding socket passing socket pointer  mnl_socket_bind (struct mnl_socket *nl, unsigned int groups, pid_t pid)
-		perror("mnl_socket_bind");//error if socket binding failed
-		exit(EXIT_FAILURE);
-	}
-	portid = mnl_socket_get_portid(nl);//obtain Netlink PortID from netlink socket
-
-	if (mnl_socket_sendto(nl, nlh, nlh->nlmsg_len) < 0) {//socket pointer, header, header length
-		perror("mnl_socket_sendto");
-		exit(EXIT_FAILURE);
-	}
-
-	// int ret;
-	// ret = mnl_socket_recvfrom(nl, buf, sizeof(buf));
-	// while (ret > 0) {
-	// 	ret = mnl_cb_run(buf, ret, seq, portid, data_cb, NULL);
-	// 	if (ret <= MNL_CB_STOP)
-	// 		break;
-	// 	ret = mnl_socket_recvfrom(nl, buf, sizeof(buf));
-	// }
-
-	// if (ret == -1) {
-	// 	perror("error");
-	// 	exit(EXIT_FAILURE);
-	// }
-
-	mnl_socket_close(nl);
-}
-
-void Network::ipShow(char *ipType) {
-	cout << "inside ip show" << endl;
-	int msgtype=RTM_GETADDR;
-	socket(ipType,msgtype);//accessing private function
-}
-
-// void Network::ipRouteAdd(char *ipType) {
-// 	cout << "inside ipRouteAdd" << endl;
-// 	int msgtype=RTM_GETADDR;
-// 	socket(ipType,msgtype);
-// }
-
-// void Network::ipRouteShow(char *ipType) {
-// 	cout << "inside ipRouteShow" << endl;
-// 	int msgtype=RTM_GETADDR;
-// 	socket(ipType,msgtype);
-// }
-
-// void Network::ipRouteDel(char *ipType) {
-// 	cout << "inside ipRouteDel" << endl;
-// 	int msgtype=RTM_GETADDR;
-// 	socket(ipType,msgtype);
-// }
-
 
 
 
 int main(int argc, char *argv[]) {
+
 	cout << "Program initiating" << endl;
 	cout << "number of Argument:" << argc << endl;
 	cout << "first argument:" << argv[0] << endl;
@@ -126,6 +20,7 @@ int main(int argc, char *argv[]) {
 
 	Network network;
 	network.ipShow(argv[1]);
+	//network.ipLink(argv[1]);
 	// network.ipRouteAdd(argv[1]);
 	// network.ipRouteShow(argv[1]);
 	// network.ipRouteDel(argv[1]);
